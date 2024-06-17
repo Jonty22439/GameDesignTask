7e89c583a0ec9b33ef1b485573144ea8b1c1856f
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-17 15:21:26 +1200

    Double Jump and Fred
    I added double jump. I also added Fred, he is awesome. I tweaked the physics to be more suitable to Fred, because I want the player to look small in the game.

--- MountainClimbingGame/Scripts/CharacterBody2D.gd
+++ MountainClimbingGame/Scripts/CharacterBody2D.gd
@@ -2,20 +2,24 @@ extends CharacterBody2D
 
 
 const SPEED = 350.0
-const JUMP_VELOCITY = -800.0
+const JUMP_VELOCITY = -650.0
+# Jumping variables for double jump
+var max_jumps = 2
+var jump_count = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.w
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	if is_on_floor():
+		jump_count = 0
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and jump_count < max_jumps:
 		velocity.y = JUMP_VELOCITY
+		jump_count = jump_count + 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
