c17d923ea47c06e163a140f629a84fca95b85a38
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-08-20 11:23:55 +1200

    Credits + grass
    I made the credits scene and I also added some grass at the lower level

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -11,3 +11,5 @@ func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/game_scene.tscn")
 
 
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/credits_scene.tscn")
