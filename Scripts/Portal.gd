85e00e1c48faac4b76033593ad833622f8163393
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-09-23 11:35:38 +1200

    Win Scene Added
    I got a win scene working so now when the player wins, they get sent to the win screen instead of the menu. From here, I can add a second level that you can access from that screen.

--- Scripts/Portal.gd
+++ Scripts/Portal.gd
@@ -10,4 +10,4 @@ func _on_body_entered(body: Node2D) -> void:
 		
 func _process(_delta):
 	if change_scene:
-		get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+		get_tree().change_scene_to_file("res://Scenes/win_scene.tscn")
