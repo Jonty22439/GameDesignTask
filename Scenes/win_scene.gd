df25824b3e5a20b69fa17bd01d0334df7e2b0c22
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-09-23 12:05:26 +1200

    More progress to new level
    I made some more progress so I can make another level soon. I still have a lot to do though...

--- Scenes/win_scene.gd
+++ Scenes/win_scene.gd
@@ -3,3 +3,7 @@ extends Node2D
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main_game.tscn")
